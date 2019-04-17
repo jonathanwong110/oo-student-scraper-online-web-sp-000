f2990ab7e17114963535a4a37d4e592a15050fb4
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-17 03:15:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,7 @@ class Student
     self.send("name=", student_hash[:name])
     self.send("location=", student_hash[:location])
     self.send("profile_url=", student_hash[:profile_url])
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
